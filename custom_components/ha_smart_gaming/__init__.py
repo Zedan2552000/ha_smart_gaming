3ff2aeb302d8d495390a4d3a54789e987090bfba
محمد أحمد محمد زيدان <95283264+Zedan2552000@users.noreply.github.com>
2026-07-14 14:19:06 +0300

    Initial OSS commit

---
+++ custom_components/ha_smart_gaming/__init__.py
@@ -0,0 +1,7 @@
+﻿"""Custom Integration"""
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the component."""
+    return True
